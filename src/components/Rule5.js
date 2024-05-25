240d990223f8460ffd23c963ab6c21f1193dbd09
rashadmin <abdul.a.rasheed2021@gmail.com>
2024-05-25 10:55:31 +0100

    increase min password length to 15

--- src/components/Rule5.js
+++ src/components/Rule5.js
@@ -5,7 +5,7 @@ import Rule6 from './Rule6'
 class Rule5 extends Component {
   render() {
     if (this.props.password){
-      const result =  this.props.password.length===12 ? 
+      const result =  this.props.password.length==12 ? 
       <>
       <Rule6 password={this.props.password} />
       <Display group='pass' name='Rule 5'>More than 12</Display>

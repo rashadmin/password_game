bc640255b6d0a3432395c490f0e6b1fa8eb5018e
rashadmin <abdul.a.rasheed2021@gmail.com>
2024-05-27 12:57:55 +0100

    removed rule 9

--- src/components/Rule9.js
+++ src/components/Rule9.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import Display from './Display'
-import Rule10 from './Rule10'
 
 class Rule9 extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       data:false
+    }
+  }
+
+  addtoState = () => {
+    fetch('https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.json')
+    .then(response=>response.json())
+    .then(content=>this.setState({data:content.includes(this.props.password)}))
+    // .catch(err=>console.log(err))
+    }
+
+  componentDidMount(){
+    this.addtoState()
+    }
   render() {
-    if (this.props.password){
-      let adds = 0 
-      this.props.password.match(/\d/g).forEach(element => adds+=parseInt(element))
-      const result =  adds===25? 
+    if (this.props.password){        
+      const result =  !this.state.data ? 
       <>
-      <Rule10 password={this.props.password} />
-      <Display group='pass' name='Rule 9'>The digits add up to 25</Display>
-      </> : 
-      <Display group='fail' name='Rule 9'>The digits does not add up to 25 e.g 9 + 7 + 6 + 3 = 25</Display>
+      <Display group='pass' name='Rule 9'>It is Not Among the Common Passwords</Display>
+      </> :  
+      <Display group='fail' name='Rule 9'>It is Among the Common Passwords</Display>
       return result
     }
 }
 }
 export default Rule9
+
+
+
+
+
